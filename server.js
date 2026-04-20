1f370b6fc2b81a5d573b34f1531b4f3d878a0ced
Shahil <sahilaryan9211@gmail.com>
2026-04-21 04:18:31 +0530

    fix: enforce absolute directories for express static assets and patch vercel rewrites

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
     secret: 'sirahi-admin-secret',
